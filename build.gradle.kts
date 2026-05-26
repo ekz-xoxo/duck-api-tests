42ad60a62a8252dc4f22f4ab7cf9344241a646b2
UlyanaNazarova <142579592+ekz-xoxo@users.noreply.github.com>
2026-05-26 21:49:58 +0700

    settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.consol.citrus:citrus-base:3.4.0")
+    testImplementation("com.consol.citrus:citrus-testng:3.4.0")
+    testImplementation("com.consol.citrus:citrus-spring:3.4.0")
+    testImplementation("com.consol.citrus:citrus-http:3.4.0 ")
+    testImplementation("com.consol.citrus:citrus-validation-json:3.4.0")
+    testImplementation("org.slf4j:slf4j-reload4j:2.0.7")
+    testImplementation("org.projectlombok:lombok:1.18.28")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
